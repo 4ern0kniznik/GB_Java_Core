b739cee088462fc2190ca802111f232ffa5ae2e7
4ern0kniznik <xooloru@gmail.com>
2019-05-23 17:21:26 +0300

    Test

--- src/Lesson_1/Marathon/Main.java
+++ src/Lesson_1/Marathon/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Course course = new Course(new Cross(80), new Wall(2), new Water(15), new Cross(120));
         Team team = new Team(new Human("Боб"), new Cat("Барсик"), new Dog("Бобик"), new Dog("Вася"));
 
-        course.doIt(team); \\\
+        course.doIt(team);
         team.showResults();
     }
 }
